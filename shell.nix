d0d3a62a3df12c5f59e570a4d5206019cd576772
Kaleb D Ruscitti <kaleb@ruscitti.ca>
2024-08-07 22:50:40 +0000

    Added pyproject.toml for packaging.

---
+++ shell.nix
@@ -0,0 +1,13 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell{
+ packages = [
+  (pkgs.python3.withPackages (python-pkgs: [
+   python-pkgs.setuptools
+   python-pkgs.build
+   ]))
+ ];
+ shellHook = ''
+ zsh
+ '';
+}
