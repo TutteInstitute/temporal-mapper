f25b183b0ae7341f7d6e713914c17155625004dc
Kaleb D Ruscitti <kaleb@ruscitti.ca>
2024-08-16 19:23:38 +0000

    Removed old build files which confused pip in the test suite?

--- build/lib/temporalmapper/__init__.py
+++
@@ -1,5 +0,0 @@
-import temporalmapper.temporal_mapper as tm_main
-
-TemporalMapper = tm_main.TemporalMapper
-centroid_datamap = tm_main.centroid_datamap
-time_semantic_plot = tm_main.time_semantic_plot
