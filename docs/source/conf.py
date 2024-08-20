f7804d2cd87fd9bb58b94e3518d48fdcde452b7d
Kaleb D Ruscitti <kaleb@ruscitti.ca>
2024-08-20 02:54:55 +0000

    Changed sphinx theme to ReadTheDocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ release = '0.3.2'
 
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
@@ -26,5 +27,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
+#html_static_path = ['_static']
