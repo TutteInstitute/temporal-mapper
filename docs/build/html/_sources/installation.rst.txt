f1a364dd633f69150ae0f268054d448ca4581bd4
Kaleb Ruscitti <kaleb@ruscitti.ca>
2024-08-26 19:35:20 +0000

    Added notebook support to docs + draft of explanatory nb

--- docs/build/html/_sources/installation.rst.txt
+++ docs/build/html/_sources/installation.rst.txt
@@ -5,6 +5,7 @@ Temporal Mapper is not yet on PyPI, so in the meantime it can be
 installed locally, as follows::
 
     git clone https://github.com/TutteInstitute/temporal-mapper.git
-    cd temporal-mapper && pip install .
-
+    cd temporal-mapper
+    pip install -r requirements.txt
+    pip install .
 
--- shell.nix
+++ shell.nix
@@ -2,11 +2,13 @@ let
   pkgs = import <nixpkgs> {};
 in pkgs.mkShell{
  packages = [
+  pkgs.pandoc
   (pkgs.python3.withPackages (python-pkgs: [
    python-pkgs.setuptools
    python-pkgs.build
    python-pkgs.sphinx
    python-pkgs.pip
+   python-pkgs.sphinx-rtd-theme
    ]))
   ];
   LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
