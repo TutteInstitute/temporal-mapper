4fb7db5bedc4cdc2d7405391cedf3401c4b9ad91
unknown <kaleb@ruscitti.ca>
2026-02-07 15:38:28 -0500

    updated __init__.py

--- src/temporalmapper/__init__.py
+++ src/temporalmapper/__init__.py
@@ -1,5 +1,6 @@
 import temporalmapper.temporal_mapper as tm_main
+import temporalmapper.utilities as tm_utils
 
 TemporalMapper = tm_main.TemporalMapper
-centroid_datamap = tm_main.centroid_datamap
-time_semantic_plot = tm_main.time_semantic_plot
+centroid_datamap = tm_utils.centroid_datamap
+time_semantic_plot = tm_utils.time_semantic_plot
